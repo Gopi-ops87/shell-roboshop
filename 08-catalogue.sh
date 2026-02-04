f622dfac153a4aa1f521e4af266b9759b21ec5b6
gopi-ops-87 <gopipasham2000@gmal.com>
2026-02-04 13:00:31 +0530

    creating db

--- 08-catalogue.sh
+++ 08-catalogue.sh
@@ -29,13 +29,9 @@ fi
 
 
 dnf module disable nodejs -y &>>$LOG_FILE
-
-
 dnf module enable nodejs:20 -y &>>$LOG_FILE
-
-
-
 dnf install nodejs -y &>>$LOG_FILE
+echo -e "Installing nodejs ... $G Success $N"
 
 id roboshop &>> $LOG_FILE
 if [ $? -ne 0 ]; then
@@ -46,40 +42,20 @@ else
 fi
 
 mkdir -p /app 
-
-
 curl -o /tmp/catalogue.zip https://roboshop-artifacts.s3.amazonaws.com/catalogue-v3.zip &>>$LOG_FILE
-
-
 cd /app 
-
-
 rm -rf /app/*
-
-
 unzip /tmp/catalogue.zip &>>$LOG_FILE
-
-
-
 npm install  &>>$LOG_FILE
-
-
 cp $SCRIPT_DIR/catalogue.service /etc/systemd/system/catalogue.service
-
-
 systemctl daemon-reload
 systemctl enable catalogue 
-
-
 systemctl start catalogue &>>$LOG_FILE
 
-
-
+echo -e "loading catalogue application ... $G Success $N"
 
 cp $SCRIPT_DIR/mongo.repo /etc/yum.repos.d/mongo.repo
-
 dnf install mongodb-mongosh -y &>>$LOG_FILE
-
 
 INDEX=$(mongosh mongodb.dev28p.online --quiet --eval "db.getMongo().getDBNames().indexOf('catalogue')")
 if [ $INDEX -le 0 ]; then
@@ -89,7 +65,7 @@ else
 fi
 
 mongosh --host $MONGODB_IP </app/db/master-data.js &>>$LOG_FILE
-
-
 systemctl restart catalogue
+
+echo -e "loading and restarting catalogue ... $G Success $N"
 
