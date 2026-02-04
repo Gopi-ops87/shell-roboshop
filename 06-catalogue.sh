b85fd0b3fe9866f85d978b0d992c9e4e35528714
gopi-ops-87 <gopipasham2000@gmal.com>
2026-02-04 11:12:04 +0530

    creating db

--- 06-catalogue.sh
+++ 06-catalogue.sh
@@ -48,7 +48,7 @@ if [ $? -ne 0 ]; then
     useradd --system --home /app --shell /sbin/nologin --comment "roboshop system user" roboshop &>>$LOG_FILE
     VALIDATE $? "creating system user"
 else
-    echo "User already exist ... $Y Skipping $N"
+    echo -e "User already exist ... $Y Skipping $N"
 fi
 
 mkdir -p /app 
