47518e9def836663b66b1c4089ab32e4aa15ad9c
gopi-ops-87 <gopipasham2000@gmal.com>
2026-02-06 19:32:24 +0530

    roboshop

--- 16-frontend.sh
+++ 16-frontend.sh
@@ -13,7 +13,7 @@ SCRIPT_DIR=$PWD
 MONGODB_HOST="mongodb.dev28p.online"
 LOG_FILE="$LOGS_FOLDER/$SCRIPT_NAME.log"  # /var/log/shell-roboshop/12-cart.log
 
-mkdir -p &>>$LOG_FOLDER
+mkdir -p &>>$LOGS_FOLDER
 
 START_TIME=$(date +%s)
 echo "script started executed at: $(date)" | tee -a &>>$LOG_FILE
